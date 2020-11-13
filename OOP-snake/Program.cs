547eb03f1d50b658b1c6a617cc69c32779baee63
Gradt <linn.doodles@gmail.com>
2020-11-13 16:40:21 +0300

    lesson-2

--- OOP-snake/Program.cs
+++ OOP-snake/Program.cs
@@ -6,8 +6,26 @@ namespace OOP_snake
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// создание переменных, хранящих координаты точек 
+			int x1 = 1;
+			int y1 = 3;
+			char sym1 = '*';
+
+			Draw(x1, y1, sym1);
+
+			int x2 = 4;
+			int y2 = 4;
+			char sym2 = '#';
+
+			Draw(x2, y2, sym2);
+
 			Console.ReadLine();
+		}
+
+		static void Draw(int x, int y, char sym)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
 		}
 	}
 }
