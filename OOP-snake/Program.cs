7275ecb3b284911b3f21a644533b2568434fd3db
Gradt <linn.doodles@gmail.com>
2020-11-16 12:23:00 +0300

    lesson-7

--- OOP-snake/Program.cs
+++ OOP-snake/Program.cs
@@ -13,45 +13,11 @@ namespace OOP_snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine xLine = new HorizontalLine(5, 10, 8, '+');
+            xLine.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            // чтобы в переменную i на каждом витке цикла были поочерёдно записаны значения всех элементов списка
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            // homework
-            Point p3 = new Point(7, 10, '%');
-            p3.Draw();
-
-            Point p4 = new Point(10, 6, '$');
-            p4.Draw();
-
-            Point p5 = new Point(13, 8, '@');
-            p5.Draw();
-
-            Point p6 = new Point(15, 20, '&');
-            p6.Draw();
-
-            List<Point> pListHomework = new List<Point>();
-            pListHomework.Add(p3);
-            pListHomework.Add(p4);
-            pListHomework.Add(p5);
-            pListHomework.Add(p6);
+            VerticalLine yLine = new VerticalLine(2, 10, 20, '+');
+            yLine.Draw();
 
             Console.ReadLine();
         }
---
+++ OOP-snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        {
+            pList = new List<Point>();
+
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
